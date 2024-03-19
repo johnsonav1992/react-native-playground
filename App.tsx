6e88eb66ebccdbe985c4dc0012ec5e00426e5673
Alex Johnson <johnsonav1992@gmail.com>
2024-03-19 05:53:09 -0500

    eslint config and made a fetch request!

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
+import {
+    QueryClient
+    , QueryClientProvider
+    , useQuery
+} from '@tanstack/react-query';
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    FlatList
+    , SafeAreaView
+    , StyleSheet
+    , Text
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const MainComp = () => {
+    const { data } = useQuery( {
+        queryKey: [ 'todos' ]
+        , queryFn: () => axios
+            .get<{ title: string }[]>( 'https://jsonplaceholder.typicode.com/todos' )
+            .then( res => res.data )
+    } );
+
+    return (
+        <SafeAreaView style={ styles.container }>
+            <Text>Hey hey!</Text>
+            <FlatList
+                data={ data }
+                renderItem={ ( { item } ) => (
+                    <Text>{ item.title }</Text>
+                ) }
+            />
+            <StatusBar style='auto' />
+        </SafeAreaView>
+    );
+};
+
+export default function App () {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={ queryClient }>
+            <MainComp />
+        </QueryClientProvider>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1
+        , backgroundColor: '#fff'
+        , alignItems: 'center'
+        , justifyContent: 'center'
+    }
+} );
